6c028ff8cf2f395960d60d5fd22c7f63c1d45bef
Polly Gannaway <polly.gannaway@gmail.com>
2014-11-12 14:29:42 +0000

    Completed pins and users associations

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,7 +1,8 @@
 class PinsController < ApplicationController
   respond_to :html, :xml, :json
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
-  before
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :correct_user, only: [:edit, :update, :destroy]
 
   def index
     @pins = Pin.all
@@ -42,6 +43,11 @@ class PinsController < ApplicationController
   private
     def set_pin
       @pin = Pin.find(params[:id])
+    end
+
+    def correct_user
+      @pin = current_user.pins.find_by(id: params[:id])
+      redirect_to pins_path, notice: "I'm afraid you aren't authorised to edit this pin." if @pin.nil?
     end
 
     def pin_params
